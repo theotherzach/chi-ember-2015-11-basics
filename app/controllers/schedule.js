9703b37c9767a07c6f003f40b98f45848a59b1dc
Zach Briggs <briggszj@gmail.com>
2015-11-02 12:27:51 -0600

    Define scheduledTime

--- app/controllers/schedule.js
+++ app/controllers/schedule.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   times: ['1:00', '1:30', '2:00'],
 
+  scheduledTime: undefined,
+
   actions: {
     selectTime: function(time) {
       this.set('scheduledTime', time);
